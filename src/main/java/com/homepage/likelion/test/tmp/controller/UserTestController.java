3774fabfd91f40947ec6e194762febc4636ce236
rtgc0915 <rtgc0915@gmail.com>
2024-05-07 21:12:27 +0900

    feat: 회원가입 API 작성
    회원가입 API 작성

--- src/main/java/com/homepage/likelion/test/tmp/controller/UserTestController.java
+++ src/main/java/com/homepage/likelion/test/tmp/controller/UserTestController.java
@@ -1,5 +1,6 @@
-package com.homepage.likelion.test;
+package com.homepage.likelion.test.tmp.controller;
 
+import com.homepage.likelion.test.tmp.dto.SignupDto;
 import com.homepage.likelion.util.response.CustomApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("tmp/api/user")
 public class UserTestController {
 
     //POST http://localhost:8081/api/user/signup
     @PostMapping("/signup")
-    public ResponseEntity<CustomApiResponse<?>> signup(@Valid e@RequestBody SignupDto dto) {
+    public ResponseEntity<CustomApiResponse<?>> signup(@Valid @RequestBody SignupDto dto) {
 
         //회원 가입 ~
         System.out.println(dto.getEmail());
